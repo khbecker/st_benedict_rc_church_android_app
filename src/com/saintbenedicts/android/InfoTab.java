1592d0eb14b58e4764e8e0be4c3c97b01d293cb7
Kevin Becker <khbecker@gmail.com>
2011-05-12 15:55:31 -0400

    Added skeleton app with tabs.

---
+++ src/com/saintbenedicts/android/InfoTab.java
@@ -0,0 +1,15 @@
+package com.saintbenedicts.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class InfoTab extends Activity {
+/** Called when the activity is first created. */
+@Override
+public void onCreate(Bundle savedInstanceState) {
+super.onCreate(savedInstanceState);
+setContentView(R.layout.infotab);
+
+}
+}
